79f5d3d67d58451008dfe7c5cb51190a8feb2bea
williameliasson-dev <williameliasson5@gmail.com>
2024-01-31 20:16:00 +0100

    fix: correct format for hour

--- src/weather/ascii_art.rs
+++ src/weather/ascii_art.rs
@@ -3,11 +3,17 @@ use chrono::Timelike;
 use super::SingleData;
 
 pub fn print_future_weather(singles: Vec<SingleData>) -> () {
-    for (i, weather) in singles.iter().enumerate() {
+    for weather in singles {
         let temp = weather.temperature_2m;
-        let hour = weather.date_time.hour();
+        let hour = weather.date_time.hour().to_string();
 
-        print!("|{hour}: ");
+        let frmt: String = if hour.len() < 2 {
+            String::from("H0:00").replace("H", &hour).to_string()
+        } else {
+            String::from("HH:00").replace("HH", &hour).to_string()
+        };
+
+        print!("|{frmt}: ");
         print!("{temp}C°");
     }
     print!("|");
